a45bb0cd1c173da3da9303f2483600f849c6da0a
T. Lanette <TLPollard4516@gmail.com>
2024-01-04 19:54:43 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,12 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Vite + React</h1>
-        <p>
-          <button onClick={() => setCount(count => count + 1)}>
-            count is {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://react.dev"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <h1>Hello</h1>
     </div>
   );
 };
