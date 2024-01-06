d08d5f37010457701676d384ec5e1ab7eaabcb50
T. Lanette <TLPollard4516@gmail.com>
2024-01-06 16:51:34 -0500

    Added styling to links on mobile menu
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,41 +3,97 @@ import { Nav, Container, NavbarBrand, NavbarCollapse, NavbarToggle, Navbar, NavL
 import { Link } from 'react-router-dom';
 
 export default function Header() {
+
+
     return (
         <header className='page-header'>
-            <Navbar collapseOnSelect expand="md" className='bg-body-dark'>
+            <Navbar collapseOnSelect expand="md" className=''>
                 <Container>
                     <NavbarBrand>
                         <h1 className='text-uppercase'>The Planets</h1>
                     </NavbarBrand>
-                    <NavbarToggle aria-controls='navbarScroll' />
-                    <NavbarCollapse id='navbarScroll'>
-                        <Nav className='me-auto'>
-                            <Link to='/mercury' className='planet-page-link text-uppercase link-underline link-underline-opacity-0' id='border-0'>
-                                <img src="/assets/mercury-circle.png" className='me-4' alt="" />
-                                <span>Mercury</span>
+                    <NavbarToggle aria-controls='navbarScroll' id='navbarBtn' className='custom-toggler' />
+                    <NavbarCollapse id='navbarCollapse'>
+                        <Nav className='me-auto' id='navbarNav'>
+                            <Link to='/mercury' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25' id='border-0'>
+                                <div>
+                                    <img src="/assets/mercury-circle.png" className='me-4' alt="" />
+                                    <span>Mercury</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
                             </Link>
-                            <Link to='/venus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/venus-circle.png" className='me-4' alt="" />
-                                <span>Venus</span></Link>
-                            <Link to='/earth' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/earth-circle.png" className='me-4' alt="" />
-                                <span>Earth</span></Link>
-                            <Link to='/mars' className='planet-page-link text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/mars-circle.png" className='me-4' alt="" />
-                                <span>Mars</span></Link>
-                            <Link to='/jupiter' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/jupiter-circle.png" className='me-4' alt="" />
-                                <span>Jupiter</span></Link>
-                            <Link to='/saturn' className='planet-page-link text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/saturn-circle.png" className='me-4' alt="" />
-                                <span>Saturn</span></Link>
-                            <Link to='/uranus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/uranus-circle.png" className='me-4' alt="" />
-                                <span>Uranus</span></Link>
-                            <Link to='/neptune' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0'>
-                                <img src="/assets/neptune-circle.png" className='me-4' alt="" />
-                                <span>Neptune</span></Link>
+
+                            <Link to='/venus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/venus-circle.png" className='me-4' alt="" />
+                                    <span>Venus</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/earth' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/earth-circle.png" className='me-4' alt="" />
+                                    <span>Earth</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/mars' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/mars-circle.png" className='me-4' alt="" />
+                                    <span>Mars</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/jupiter' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/jupiter-circle.png" className='me-4' alt="" />
+                                    <span>Jupiter</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/saturn' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/saturn-circle.png" className='me-4' alt="" />
+                                    <span>Saturn</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/uranus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/uranus-circle.png" className='me-4' alt="" />
+                                    <span>Uranus</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
+
+                            <Link to='/neptune' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
+                                <div>
+                                    <img src="/assets/neptune-circle.png" className='me-4' alt="" />
+                                    <span>Neptune</span>
+                                </div>
+                                <div>
+                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                </div>
+                            </Link>
                         </Nav>
                     </NavbarCollapse>
                 </Container>
