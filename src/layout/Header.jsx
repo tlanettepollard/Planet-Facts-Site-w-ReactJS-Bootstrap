df0725ec84e3aa9b4b2ae7b101af8cdbda8ac169
T. Lanette <TLPollard4516@gmail.com>
2024-01-06 17:06:21 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,90 +8,90 @@ export default function Header() {
     return (
         <header className='page-header'>
             <Navbar collapseOnSelect expand="md" className=''>
-                <Container>
+                <Container className='d-md-flex flex-md-column'>
                     <NavbarBrand>
                         <h1 className='text-uppercase'>The Planets</h1>
                     </NavbarBrand>
                     <NavbarToggle aria-controls='navbarScroll' id='navbarBtn' className='custom-toggler' />
                     <NavbarCollapse id='navbarCollapse'>
-                        <Nav className='me-auto' id='navbarNav'>
+                        <Nav className='me-auto d-md-flex flex-md-row' id='navbarNav'>
                             <Link to='/mercury' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25' id='border-0'>
                                 <div>
-                                    <img src="/assets/mercury-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/mercury-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Mercury</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/venus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/venus-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/venus-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Venus</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/earth' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/earth-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/earth-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Earth</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/mars' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/mars-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/mars-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Mars</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/jupiter' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/jupiter-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/jupiter-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Jupiter</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/saturn' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/saturn-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/saturn-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Saturn</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/uranus' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/uranus-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/uranus-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Uranus</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
 
                             <Link to='/neptune' className='planet-page-link  text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25'>
                                 <div>
-                                    <img src="/assets/neptune-circle.png" className='me-4' alt="" />
+                                    <img src="/assets/neptune-circle.png" className='me-4 d-md-none' alt="" />
                                     <span>Neptune</span>
                                 </div>
                                 <div>
-                                    <img src="/assets/icon-chevron.svg" alt="" />
+                                    <img className='d-md-none' src="/assets/icon-chevron.svg" alt="" />
                                 </div>
                             </Link>
                         </Nav>
