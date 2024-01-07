dc7fadd143faf30298297e2836d9d458fbe265b0
T. Lanette <TLPollard4516@gmail.com>
2024-01-06 19:07:23 -0500

    Update: navbar for desktop
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -8,13 +8,13 @@ export default function Header() {
     return (
         <header className='page-header'>
             <Navbar collapseOnSelect expand="md" className=''>
-                <Container className='d-md-flex flex-md-column'>
+                <Container className='d-md-flex flex-md-column flex-lg-row justify-content-lg-between'>
                     <NavbarBrand>
                         <h1 className='text-uppercase pb-md-3'>The Planets</h1>
                     </NavbarBrand>
                     <NavbarToggle aria-controls='navbarScroll' id='navbarBtn' className='custom-toggler' />
-                    <NavbarCollapse id='navbarCollapse'>
-                        <Nav className='me-auto d-md-inline-flex justify-content-md-center align-items-md-start gap-4 pe-md-2' id='navbarNav'>
+                    <NavbarCollapse id='navbarCollapse' className='flex-lg-grow-0'>
+                        <Nav className='me-auto d-md-inline-flex justify-content-md-center align-items-md-start gap-4 pe-md-2 pe-lg-0' id='navbarNav'>
                             <Link to='/mercury' className='planet-page-link text-uppercase link-underline link-underline-opacity-0 d-inline-flex justify-content-between align-items-center border-bottom border-secondary border-1 border-opacity-25' id='border-0'>
                                 <div>
                                     <img src="/assets/mercury-circle.png" className='me-4 d-md-none' alt="" />
