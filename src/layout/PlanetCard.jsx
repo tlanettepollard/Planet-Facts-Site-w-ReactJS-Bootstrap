62e2d3791042e39388bf4439c70e9fa32a8f7e5e
T. Lanette <TLPollard4516@gmail.com>
2024-01-06 21:15:55 -0500

    Added more information to data file
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/PlanetCard.jsx
+++ src/layout/PlanetCard.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function PlanetCard() {
+export default function PlanetCard(props) {
+
     return (
-        <>
-            <h1>Here's my Planet Facts Card!</h1>
-        </>
+        <div className='planet-card'>
+            <MainInfoCard />
+            <MainDataList />
+        </div>
     );
+}
+
+// Get information about each planet
+const MainInfoCard = (props) => {
+    const [name, setName] = useState(props.name);
+    const [text, setText] = useState(props.overview.content);
+    const [imageUrl, setImageUrl] = useState(props.images.planet);
+    const [surfaceImage, setSurfaceImage] = useState(props.images.geology);
+    const [sourceLink, setSourceLink] = useState(props.overview.source);
+    const [showSurfaceImage, setShowSurfaceImage] = useState(false);
+    const [color, setColor] = useState()
 }
