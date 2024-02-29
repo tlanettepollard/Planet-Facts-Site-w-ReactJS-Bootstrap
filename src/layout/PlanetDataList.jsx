499f55f6600561e7024da00674b2ea36820adcdd
T. Lanette <TLPollard4516@gmail.com>
2024-02-28 19:44:54 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/PlanetDataList.jsx
+++ src/layout/PlanetDataList.jsx
@@ -7,17 +7,17 @@ export default function PlanetDataList(props) {
     return (
         <Container className='facts-container'>
             <div className='planet-card__data'>
-                <Row className="gx-0">
-                    <Col sm={12} md={3}>
+                <Row className="">
+                    <Col sm={12} md={3} lg={3}>
                         <DataListItem name='rotation time' value={props.planet.rotation} className='' />
                     </Col>
-                    <Col sm={12} md={3}>
+                    <Col sm={12} md={3} lg={3}>
                         <DataListItem name='revolution time' value={props.planet.revolution} className='' />
                     </Col>
-                    <Col sm={12} md={3}>
+                    <Col sm={12} md={3} lg={3}>
                         <DataListItem name='radius' value={props.planet.radius} className='' />
                     </Col>
-                    <Col sm={12} md={3}>
+                    <Col sm={12} md={3} lg={3}>
                         <DataListItem name='average temp.' value={props.planet.temperature} className='' />
                     </Col>
                 </Row>
