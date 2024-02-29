a0482fa3c8dcb709c44cf099ba4d377cd9a743c8
T. Lanette <TLPollard4516@gmail.com>
2024-02-28 20:07:43 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <section className="attribution text-center py-4 fs-6">
+        <section className="attribution text-center mt-lg-5 py-4 fs-6">
             <span className='text-secondary'>Challenge by </span>
             <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className='link-secondary'>
                 Frontend Mentor
