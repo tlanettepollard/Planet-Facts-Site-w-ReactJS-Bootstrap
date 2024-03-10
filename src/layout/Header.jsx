0e14ef647740085e1cc7e017df776aef74990db1
T. Lanette <TLPollard4516@gmail.com>
2024-03-09 21:22:42 -0500

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,9 +6,9 @@ export default function Header() {
 
 
     return (
-        <header className='page-header container'>
-            <Navbar collapseOnSelect expand="md" className='navbar-dark pb-md-5 pt-lg-5 pb-lg-4 pt-xl-2 pb-xl-2 borderBottom'>
-                <Container className='d-md-flex flex-md-column flex-xl-row justify-content-lg-between'>
+        <header className='page-header container-fluid'>
+            <Navbar collapseOnSelect expand="md" className='navbar-dark pb-md-5 pt-lg-5 pb-lg-4 pt-xl-2 pb-xl-2 borderBottom container-fluid'>
+                <Container className='d-md-flex flex-md-column flex-xl-row justify-content-lg-between' fluid>
                     <NavbarBrand>
                         <h1 className='text-uppercase pb-md-3 pt-lg-3 page-title'>The Planets</h1>
                     </NavbarBrand>
