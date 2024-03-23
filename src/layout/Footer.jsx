52eb4dbf07c9bfc9f9c740d87caa5d3f020e0bda
T. Lanette <TLPollard4516@gmail.com>
2024-03-23 10:53:37 -0400

    Updates
    Signed-off-by: T. Lanette <TLPollard4516@gmail.com>

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <section className="attribution text-center mt-lg-5 mt-xl-2 py-4 fs-6">
-            <h6 className='text-secondary'>Challenge by </h6>
+        <section role='contentinfo' className="attribution text-center mt-lg-5 mt-xl-2 py-4 fs-6">
+            <h1 className='text-secondary fs-6'>Challenge by </h1>
             <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className='link-secondary'>
                 Frontend Mentor
             </a>
